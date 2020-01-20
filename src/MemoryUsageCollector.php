6a4ad123c802651c0eeba7499b2420c80fdc6535
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-01-20 21:32:55 +0100

    Updated to latest VO's

--- src/MemoryUsageCollector.php
+++ src/MemoryUsageCollector.php
@@ -6,8 +6,10 @@ use function ApiClients\Tools\Rx\observableFromArray;
 use ReactInspector\CollectorInterface;
 use ReactInspector\Config;
 use ReactInspector\Measurement;
+use ReactInspector\Measurements;
 use ReactInspector\Metric;
 use ReactInspector\Tag;
+use ReactInspector\Tags;
 use Rx\Observable;
 
 final class MemoryUsageCollector implements CollectorInterface
@@ -21,29 +23,37 @@ final class MemoryUsageCollector implements CollectorInterface
                     'gauge',
                     ''
                 ),
-                [],
-                [
+                new Tags(),
+                new Measurements(
                     new Measurement(
                         \memory_get_usage(true),
-                        new Tag('perspective', 'external'),
-                        new Tag('peak', 'false')
+                        new Tags(
+                            new Tag('perspective', 'external'),
+                            new Tag('peak', 'false')
+                        )
                     ),
                     new Measurement(
                         \memory_get_peak_usage(true),
-                        new Tag('perspective', 'external'),
-                        new Tag('peak', 'true')
+                        new Tags(
+                            new Tag('perspective', 'external'),
+                            new Tag('peak', 'true')
+                        )
                     ),
                     new Measurement(
                         \memory_get_usage(false),
-                        new Tag('perspective', 'internal'),
-                        new Tag('peak', 'false')
+                        new Tags(
+                            new Tag('perspective', 'internal'),
+                            new Tag('peak', 'false')
+                        )
                     ),
                     new Measurement(
                         \memory_get_peak_usage(false),
-                        new Tag('perspective', 'internal'),
-                        new Tag('peak', 'true')
+                        new Tags(
+                            new Tag('perspective', 'internal'),
+                            new Tag('peak', 'true')
+                        )
                     ),
-                ]
+                )
             ),
         ]);
     }
